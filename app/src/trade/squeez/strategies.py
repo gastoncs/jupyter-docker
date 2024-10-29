9ab71fe362ba1cead26d63a787ca30719fd02ac8
Gaston Cortes <gastoncortess@gmail.com>
2024-10-29 15:15:03 +0100

    Adapt to the real trade

--- app/src/trade/squeez/strategies.py
+++ app/src/trade/squeez/strategies.py
@@ -14,7 +14,6 @@ transaction_number = 0
 def squeez(df, window=1):
 
     df = df.copy()
-
     df = init(df)
     calculateBoolingerAndKeltnerChannels(df, kc)
     detectSqueeze(df)
@@ -29,7 +28,7 @@ def squeez(df, window=1):
     df['price']=price
     df['transaction_number']=transaction_number
 
-    df = df[['bar','symbol','date_est','time_est','high','low','close','volume','open','close_smooth','ema25',
+    df = df[['symbol','date_est','time_est','high','low','close','open','close_smooth','ema25',
              'squeezed_area','squeeze_close_ema','is_the_day_above_25ema','last_tree_under','last_tree_over',
              'posible_entry','qty','position','price','transaction_number']] 
     
@@ -40,19 +39,20 @@ def init(df):
     ''' Create columns if does not exist
     '''
     cols_to_check = ['high','low','close','volume','open','position','last_tree_under']
-    
     new_list =list(set(df.columns).union(cols_to_check))
-    df = df.reindex(columns=sorted(new_list)).fillna(0)
-    
-    df['symbol'] = 'GOOGLE'
+
+    df['symbol'] = 'NVDA.US_9'
     df['qty'] = 10
+    df['time'] = df.index
+
+    df.reset_index(inplace=True, drop=True)
+    
     df['ema25'] = df['close'].ewm(span=25, adjust=False).mean()
     df["close_smooth"] = savgol_filter(df.close, 49, 5)
     df["high_smooth"] = savgol_filter(df.high, 49, 5)
     df["low_smooth"] = savgol_filter(df.low, 49, 5)
+    df['est']=pd.to_datetime(df['time'], unit='ms').dt.tz_localize('UTC').dt.tz_convert('US/Eastern')
     df['YMD'] = df['est'].dt.strftime('%Y%m%d')
-    #estTime = pd.to_datetime(df.index, unit='ms').tz_localize('UTC').tz_convert('US/Eastern')
-    #df["est"] = estTime.strftime('%Y-%m-%d %H:%M:%S')
     df['date_est']=df['est'].dt.date
     df['time_est']=df['est'].dt.time
     
